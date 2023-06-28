445660959fafe093cbff0017295b90162a67146b
Ernest Bracy <ernestbracy1@gmail.com>
2023-06-28 11:01:07 -0400

    Created an state array for states to be added into

---
+++ script.js
@@ -0,0 +1,43 @@
+// Created an array of states (Starting with 13 Colonies)
+const states = [
+  {
+    state: "Connecticut"
+  },
+  {
+    state: "Maryland"
+  },
+  {
+    state: "New York"
+  },
+  {
+    state: "New Hampshire"
+  },
+  {
+    state: "South Carolina"
+  },
+  {
+    state: "Virginia"
+  },
+  {
+    state: "Delaware"
+  },
+  {
+    state: "Georgia"
+  },
+  {
+    state: "Rhode Island"
+  },
+  {
+    state: "Massachusetts"
+  },
+  {
+    state: "Pennsylvania"
+  },
+  {
+    state: "New Jersey"
+  },
+  {
+    state: "North Carolina"
+  },
+]
+
